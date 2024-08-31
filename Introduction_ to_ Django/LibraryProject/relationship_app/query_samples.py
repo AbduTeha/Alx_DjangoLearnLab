2cda9c5e941cf41e66e6ab3ecd8c360e4c9c13b8
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-08-31 22:10:36 +0300

    \

---
+++ Introduction_ to_ Django/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,14 @@
+from django.db.models import Q
+from .models import Book, Library, Librarian
+
+# Query all books by a specific author
+def get_books_by_author(author_name):
+    return Book.objects.filter(author__name=author_name)
+
+# List all books in a library
+def get_books_in_library(library_name):
+    return Book.objects.filter(library__name=library_name)
+
+# Retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    return Librarian.objects.get(library__name=library_name)
