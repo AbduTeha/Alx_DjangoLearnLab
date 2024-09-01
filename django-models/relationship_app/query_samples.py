8fbcdf4a59db0d9cfd2bee320ceda011e1542a4a
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-09-01 06:35:10 +0300

    update on reationship_app

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,14 +1,19 @@
-from django.db.models import Q
-from .models import Book, Library, Librarian
+from .models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def get_books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    return books
 
 # List all books in a library
 def get_books_in_library(library_name):
-    return Book.objects.filter(library__name=library_name)
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    return books
 
 # Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    return Librarian.objects.get(library__name=library_name)
+def get_librarian(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = library.librarian
+    return librarian
