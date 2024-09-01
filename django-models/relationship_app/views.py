b10b500d08e06b52f67ef0d73e95f459b0e56594
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-09-01 17:12:46 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -56,8 +56,15 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
 def is_admin(user):
     return user.userprofile.role == 'Admin'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_template.html')
+
 
 def is_librarian(user):
     return user.userprofile.role == 'Librarian'
@@ -65,15 +72,6 @@ def is_librarian(user):
 def is_member(user):
     return user.userprofile.role == 'Member'
 
-
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
-
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return user.userprofile.role == 'Member'
 
 @user_passes_test(is_admin)
 def admin_view(request):
