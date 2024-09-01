9ed48eb0b9201bf077d019f331e64fb64c421bcb
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-09-01 19:32:41 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import CustomUser
 
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=255)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
+
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
