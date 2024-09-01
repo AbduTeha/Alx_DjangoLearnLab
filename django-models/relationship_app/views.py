916eb5194a5c4c12c4063c32e954fc222167eb2a
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-09-01 11:07:11 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,8 +8,12 @@ from .models import Library
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html')
-# relationship_app/views.py
 
+
+
+def library_detail(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/library_detail.html')
 class LibraryDetailView(generic.DetailView):
     model = Library
     template_name = 'library_detail.html'
