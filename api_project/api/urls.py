b41af3bfd7d1dd236462c5950e357b8f986b8b1d
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-09-01 21:16:26 +0300

    fa

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,14 @@
 # api/urls.py
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('books/', views.BookList.as_view(), name='book_list'),
+    path('', include(router.urls)),
 ]
+router = DefaultRouter()
+router.register(r'books', views.BookViewSet, basename='books')
+
+
+    
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,7 +6,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
 
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
