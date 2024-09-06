34fa733697ec564f7bac58e3d0792e0706e5f20c
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-09-06 21:17:50 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,7 +7,7 @@ from .serializers import BookSerializer
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .filters import BookFilter, django_filters
 from django_filters.rest_framework import DjangoFilterBackend
-
+from django_filters import rest_framework
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
