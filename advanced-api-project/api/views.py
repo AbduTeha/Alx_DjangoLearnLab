6f4e12d366d4bb320354ad20350b28440ac2d48a
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-09-07 10:56:06 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -8,6 +8,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from .filters import BookFilter, django_filters
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import rest_framework
+from rest_framework.filters import OrderingFilter
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
