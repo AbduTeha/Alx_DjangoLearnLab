d9ea068e92c177464f232eeb80809e5f59594b8d
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-09-08 06:50:30 +0300

    Create test_views.py

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,59 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import Book
+from .serializers import BookSerializer
+
+class BookAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.book_data = {
+            'title': 'Test Book',
+            'author': 'Test Author',
+            'publication_year': 2023
+        }
+        self.book = Book.objects.create(**self.book_data)
+        self.url = '/api/books/'
+
+    def test_create_book(self):
+        response = self.client.post(self.url, self.book_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 2)
+        self.assertEqual(Book.objects.latest('id').title, self.book_data['title'])
+
+    def test_read_book(self):
+        response = self.client.get(f'{self.url}{self.book.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], self.book.title)
+
+    def test_update_book(self):
+        update_data = {'title': 'Updated Title'}
+        response = self.client.put(f'{self.url}{self.book.id}/', update_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.title, update_data['title'])
+
+    def test_delete_book(self):
+        response = self.client.delete(f'{self.url}{self.book.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
+
+    def test_filter_books(self):
+        response = self.client.get(f'{self.url}?title=Test Book')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+
+    def test_search_books(self):
+        response = self.client.get(f'{self.url}?search=Test')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 1)
+
+    def test_order_books(self):
+        response = self.client.get(f'{self.url}?ordering=title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'][0]['title'], self.book.title)
+
+    def test_authentication_required(self):
+        self.client.logout()
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
