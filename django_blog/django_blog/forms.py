5a8fffada15ccb3693ac382bdf0b3fcc273ba819
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-09-13 08:04:46 +0300

    Update forms.py

--- django_blog/django_blog/forms.py
+++ django_blog/django_blog/forms.py
@@ -1,17 +1,16 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from taggit.forms import TagWidget
-
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
 class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
     message = forms.CharField(widget=forms.Textarea)
-    tag = forms.CharField(widget=TagWidget)
+    tags = forms.CharField(widget=TagWidget)
