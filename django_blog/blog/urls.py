8eac32abbce4ba27cda39aefd2ff8c8ba04d4a89
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-09-13 07:44:51 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     path('posts/<int:post_pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
     path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment_update'),
     path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
