b98115c0cf0dc2c44458f2216ebf8ea258c3d46f
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-09-22 21:50:31 +0300

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,8 +4,8 @@ from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework import status
 from .models import CustomUser
-
-
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(APIView):
     def post(self, request):
@@ -19,18 +19,17 @@ class TokenView(APIView):
     def get(self, request):
         pass
 
+class FollowView(GenericAPIView):
+    permission_classes = [IsAuthenticated]
 
-
-
-
-
-class FollowView(APIView):
     def post(self, request, user_id):
         user_to_follow = CustomUser.objects.get(id=user_id)
         request.user.following.add(user_to_follow)
         return Response(status=status.HTTP_201_CREATED)
 
-class UnfollowView(APIView):
+class UnfollowView(GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, user_id):
         user_to_unfollow = CustomUser.objects.get(id=user_id)
         request.user.following.remove(user_to_unfollow)
