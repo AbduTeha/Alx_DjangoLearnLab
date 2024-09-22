9d9aeaabe1937739dd4b63ba613fe0bd89eb043d
Abdulwahid Teha Awel <abdtu1194@gmail.com>
2024-09-22 21:30:19 +0300

    created a new app called posts

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'title', 'content', 'created_at', 'updated_at']
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'content', 'created_at', 'updated_at']
---
+++ social_media_api/posts/ursl.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import PostViewSet, CommentViewSet
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'comments', CommentViewSet, basename='comments')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
